25e3acebbb3e99f00d5c58c621e3087abbbb619a
EQIX-TAM-JP <snakayama@ap.equinix.com>
2024-09-19 10:15:43 +0900

    removing unnecessary part

--- main.tf
+++ main.tf
@@ -30,12 +30,6 @@ provider "iosxe" {
   url      = "https://${data.terraform_remote_state.ne.outputs.ssh_ip_vd_sec}"
 }
 
-# data "equinix_network_device" "vd_pri" {
-#   name = "vd-${var.metro_code}-${var.username}-pri"
-# }
-# data "equinix_network_device" "vd_sec" {
-#   name = "vd-${var.sec_metro_code}-${var.username}-sec"
-# }
 data "terraform_remote_state" "ne" {
   backend = "remote"
   config = {
@@ -56,12 +50,10 @@ data "terraform_remote_state" "bgp" {
 }
 
 locals {
-  ipv4_mg_pri   = cidrhost(data.terraform_remote_state.bgp.outputs.vrf_ranges[1], 1)
-  ipv4_mg_sec   = cidrhost(data.terraform_remote_state.bgp.outputs.vrf_ranges_sec[1], 1)
-  ipv4_pri      = cidrhost(data.terraform_remote_state.bgp.outputs.vrf_ranges[1], 2)
-  ipv4_sec      = cidrhost(data.terraform_remote_state.bgp.outputs.vrf_ranges_sec[1], 2)
-  # vlan4peer_pri = [for z in equinix_fabric_connection.vd2mg_pri.z_side : [for ap in z.access_point : [for lp in ap.link_protocol : lp.vlan_tag]]][0][0][0]
-  # vlan4peer_sec = [for z in equinix_fabric_connection.vd2mg_sec.z_side : [for ap in z.access_point : [for lp in ap.link_protocol : lp.vlan_tag]]][0][0][0]
+  ipv4_vrf_pri   = cidrhost(data.terraform_remote_state.bgp.outputs.vrf_ranges[1], 1)
+  ipv4_vrf_sec   = cidrhost(data.terraform_remote_state.bgp.outputs.vrf_ranges_sec[1], 1)
+  ipv4_int_pri      = cidrhost(data.terraform_remote_state.bgp.outputs.vrf_ranges[1], 2)
+  ipv4_int_sec      = cidrhost(data.terraform_remote_state.bgp.outputs.vrf_ranges_sec[1], 2)
 }
 
 # IOS-XE configuration
@@ -75,12 +67,11 @@ resource "iosxe_interface_ethernet" "interface_pri" {
   ip_proxy_arp                     = false
   ip_redirects                     = false
   ip_unreachables                  = false
-  ipv4_address                     = local.ipv4_pri
-  ipv4_address_mask                = cidrnetmask("${local.ipv4_pri}/${data.terraform_remote_state.bgp.outputs.cidr}")
+  ipv4_address                     = local.ipv4_int_pri
+  ipv4_address_mask                = cidrnetmask("${local.ipv4_int_pri}/${data.terraform_remote_state.bgp.outputs.cidr}")
   snmp_trap_link_status            = true
   logging_event_link_status_enable = true
 }
-
 resource "iosxe_interface_ethernet" "interface_sec" {
   provider                         = iosxe.vd_sec
   type                             = "GigabitEthernet"
@@ -91,58 +82,12 @@ resource "iosxe_interface_ethernet" "interface_sec" {
   ip_proxy_arp                     = false
   ip_redirects                     = false
   ip_unreachables                  = false
-  ipv4_address                     = local.ipv4_sec
-  ipv4_address_mask                = cidrnetmask("${local.ipv4_sec}/${data.terraform_remote_state.bgp.outputs.cidr}")
+  ipv4_address                     = local.ipv4_int_sec
+  ipv4_address_mask                = cidrnetmask("${local.ipv4_int_sec}/${data.terraform_remote_state.bgp.outputs.cidr}")
   snmp_trap_link_status            = true
   logging_event_link_status_enable = true
 }
 
-# resource "iosxe_interface_ethernet" "interface_sec1" {
-#   provider                         = iosxe.vd_pri
-#   type                             = "GigabitEthernet"
-#   name                             = var.int_sec
-#   bandwidth                        = var.bw
-#   description                      = var.int_desc_sec
-#   shutdown                         = false
-#   ip_proxy_arp                     = false
-#   ip_redirects                     = false
-#   ip_unreachables                  = false
-#   ipv4_address                     = local.ipv4_sec1
-#   ipv4_address_mask                = cidrnetmask("${local.ipv4_sec1}/${data.terraform_remote_state.bgp.outputs.cidr}")
-#   snmp_trap_link_status            = true
-#   logging_event_link_status_enable = true
-# }
-
-# resource "iosxe_interface_ethernet" "interface_pri2" {
-#   provider                         = iosxe.vd_sec
-#   type                             = "GigabitEthernet"
-#   name                             = var.int_pri
-#   bandwidth                        = var.bw
-#   description                      = var.int_desc_pri
-#   shutdown                         = false
-#   ip_proxy_arp                     = false
-#   ip_redirects                     = false
-#   ip_unreachables                  = false
-#   ipv4_address                     = local.ipv4_pri2
-#   ipv4_address_mask                = cidrnetmask("${local.ipv4_pri2}/${data.terraform_remote_state.bgp.outputs.cidr}")
-#   snmp_trap_link_status            = true
-#   logging_event_link_status_enable = true
-# }
-# resource "iosxe_interface_ethernet" "interface_sec2" {
-#   provider                         = iosxe.vd_sec
-#   type                             = "GigabitEthernet"
-#   name                             = var.int_sec
-#   bandwidth                        = var.bw
-#   description                      = var.int_desc_sec
-#   shutdown                         = false
-#   ip_proxy_arp                     = false
-#   ip_redirects                     = false
-#   ip_unreachables                  = false
-#   ipv4_address                     = local.ipv4_sec2
-#   ipv4_address_mask                = cidrnetmask("${local.ipv4_sec2}/${data.terraform_remote_state.bgp.outputs.cidr}")
-#   snmp_trap_link_status            = true
-#   logging_event_link_status_enable = true
-# }
 resource "iosxe_bgp" "bgp_pri" {
   provider             = iosxe.vd_pri
   asn                  = var.vnf_asn
@@ -157,7 +102,7 @@ resource "iosxe_bgp" "bgp_sec" {
 resource "iosxe_bgp_neighbor" "neighbor_pri" {
   provider                = iosxe.vd_pri
   asn                     = var.vnf_asn
-  ip                      = local.ipv4_mg_pri
+  ip                      = local.ipv4_vrf_pri
   remote_as               = data.terraform_remote_state.bgp.outputs.vrf_asn
   description             = var.neighbor_desc_pri
   shutdown                = false
@@ -168,44 +113,13 @@ resource "iosxe_bgp_neighbor" "neighbor_pri" {
 resource "iosxe_bgp_neighbor" "neighbor_sec" {
   provider                = iosxe.vd_sec
   asn                     = var.vnf_asn
-  ip                      = local.ipv4_mg_sec
+  ip                      = local.ipv4_vrf_sec
   remote_as               = data.terraform_remote_state.bgp.outputs.vrf_asn
   description             = var.neighbor_desc_sec
   shutdown                = false
   disable_connected_check = false
   log_neighbor_changes    = true
 }
-
-# resource "iosxe_bgp_neighbor" "neighbor_sec1" {
-#   provider                = iosxe.vd_pri
-#   asn                     = var.vnf_asn
-#   ip                      = local.ipv4_mg_sec
-#   remote_as               = data.terraform_remote_state.bgp.outputs.vrf_asn
-#   description             = var.neighbor_desc_sec
-#   shutdown                = false
-#   disable_connected_check = false
-#   log_neighbor_changes    = true
-# }
-# resource "iosxe_bgp_neighbor" "neighbor_pri2" {
-#   provider                = iosxe.vd_sec
-#   asn                     = var.vnf_asn
-#   ip                      = local.ipv4_mg_pri
-#   remote_as               = data.terraform_remote_state.bgp.outputs.vrf_asn
-#   description             = var.neighbor_desc_pri
-#   shutdown                = false
-#   disable_connected_check = false
-#   log_neighbor_changes    = true
-# }
-# resource "iosxe_bgp_neighbor" "neighbor_sec2" {
-#   provider                = iosxe.vd_sec
-#   asn                     = var.vnf_asn
-#   ip                      = local.ipv4_mg_sec
-#   remote_as               = data.terraform_remote_state.bgp.outputs.vrf_asn
-#   description             = var.neighbor_desc_sec
-#   shutdown                = false
-#   disable_connected_check = false
-#   log_neighbor_changes    = true
-# }
 
 # resource "iosxe_save_config" "write_pri" {
 #   provider = iosxe.vd_pri
@@ -233,9 +147,7 @@ resource "equinix_fabric_connection" "vd2vrf_pri" {
     emails = var.emails
   }
   bandwidth = 50
-  # order {
-  #   purchase_order_number = "1-323292"
-  # }
+
   a_side {
     access_point {
       type = "VD"
@@ -267,9 +179,7 @@ resource "equinix_fabric_connection" "vd2vrf_sec" {
     emails = var.emails
   }
   bandwidth = 50
-  # order {
-  #   purchase_order_number = "1-323292"
-  # }
+
   a_side {
     access_point {
       type = "VD"
@@ -290,13 +200,15 @@ resource "equinix_fabric_connection" "vd2vrf_sec" {
   }
 }
 
-# resource "time_sleep" "wait_2_min" {
-#   create_duration = "2m"
-#   depends_on = [
-#     equinix_fabric_connection.vd2mg_pri,
-#     equinix_fabric_connection.vd2mg_sec
-#   ]
-# }
+/*
+resource "time_sleep" "wait_2_min" {
+  create_duration = "2m"
+  depends_on = [
+    equinix_fabric_connection.vd2vrf_pri,
+    equinix_fabric_connection.vd2vrf_sec
+  ]
+}
+*/
 
 resource "equinix_metal_connection" "vrf2vd" {
   name          = var.connection_name
@@ -312,30 +224,30 @@ resource "equinix_metal_connection" "vrf2vd" {
   service_token_type = "z_side"
 }
 
-# resource "equinix_metal_virtual_circuit" "peer_pri" {
-#   project_id = var.project_id
-#   # virtual_circuit_id = equinix_fabric_connection.vd2mg_pri.uuid
-#   connection_id = equinix_metal_connection.vrf2vd.id
-#   port_id       = equinix_metal_connection.vrf2vd.ports[0].id
-#   vrf_id        = data.terraform_remote_state.bgp.outputs.vrf_pri
-#   peer_asn      = var.vnf_asn
-#   subnet        = cidrsubnet(data.terraform_remote_state.bgp.outputs.vrf_ranges[1], 5, 0)
-#   metal_ip      = local.ipv4_mg_pri
-#   customer_ip   = local.ipv4_pri
-#   nni_vlan      = [for z in equinix_fabric_connection.vd2mg_pri.z_side : [for ap in z.access_point : [for lp in ap.link_protocol : lp.vlan_tag]]][0][0][0]
-#   depends_on    = [time_sleep.wait_2_min,
-#   equinix_fabric_connection.vd2mg_pri]
-# }
-# resource "equinix_metal_virtual_circuit" "peer_sec" {
-#   project_id         = var.project_id
-#   # virtual_circuit_id = equinix_fabric_connection.vd2mg_sec.uuid
-#   connection_id = equinix_metal_connection.vrf2vd.id
-#   port_id    = equinix_metal_connection.vrf2vd.ports[1].id
-#   vrf_id     = data.terraform_remote_state.bgp.outputs.vrf_sec
-#   peer_asn   = var.vnf_asn
-#   subnet     = cidrsubnet(data.terraform_remote_state.bgp.outputs.vrf_ranges_sec[1], 5, 0)
-#   metal_ip = local.ipv4_mg_sec
-#   customer_ip = local.ipv4_sec
-#   nni_vlan = local.vlan4peer_sec
-#   depends_on = [time_sleep.wait_2_min]
-# }
+/* commented out portion of BGP peering setup for now -TF not supported-
+resource "equinix_metal_virtual_circuit" "peer_pri" {
+  project_id = var.project_id
+  connection_id = equinix_metal_connection.vrf2vd.id
+  port_id       = equinix_metal_connection.vrf2vd.ports[0].id
+  vrf_id        = data.terraform_remote_state.bgp.outputs.vrf_pri
+  peer_asn      = var.vnf_asn
+  subnet        = cidrsubnet(data.terraform_remote_state.bgp.outputs.vrf_ranges[1], 5, 0)
+  metal_ip      = local.ipv4_mg_pri
+  customer_ip   = local.ipv4_pri
+  nni_vlan      = [for z in equinix_fabric_connection.vd2vrf_pri.z_side : [for ap in z.access_point : [for lp in ap.link_protocol : lp.vlan_tag]]][0]
+  depends_on    = [time_sleep.wait_2_min,
+  equinix_fabric_connection.vd2vrf_pri]
+}
+resource "equinix_metal_virtual_circuit" "peer_sec" {
+  project_id         = var.project_id
+  connection_id = equinix_metal_connection.vrf2vd.id
+  port_id    = equinix_metal_connection.vrf2vd.ports[1].id
+  vrf_id     = data.terraform_remote_state.bgp.outputs.vrf_sec
+  peer_asn   = var.vnf_asn
+  subnet     = cidrsubnet(data.terraform_remote_state.bgp.outputs.vrf_ranges_sec[1], 5, 0)
+  metal_ip = local.ipv4_mg_sec
+  customer_ip = local.ipv4_sec
+  nni_vlan = [for z in equinix_fabric_connection.vd2vrf_pri.z_side : [for ap in z.access_point : [for lp in ap.link_protocol : lp.vlan_tag]]][0]
+  depends_on = [time_sleep.wait_2_min]
+}
+*/
