92a562e40cb0c8399d3ae8091b7c2c87c36edbaf
Vipin Vikraman <vvikraman@ap.equinix.com>
2024-10-01 11:59:59 +0800

    added layer3 config automation for FCR and AWS VIF

--- fcr.tf
+++ fcr.tf
@@ -1,4 +1,4 @@
-/*##vipin - module to spin up FCR
+##vipin - module to spin up FCR
 data "terraform_remote_state" "fcr_id" {
   backend = "remote"
 
@@ -49,8 +49,98 @@ resource "equinix_fabric_connection" "L2_FCRSG_to_AWS" {
   }
 }
 
+#vipin - to fecth the AWS Dx connection id
+locals {
+
+
+  z_side_list = [
+    for z in equinix_fabric_connection.L2_FCRSG_to_AWS.z_side : 
+    tolist(z.access_point)[0]
+   
+  ]
+
+    provider_connection_ids = [
+    for access_point in [
+      for z in equinix_fabric_connection.L2_FCRSG_to_AWS.z_side : tolist(z.access_point)[0]
+    ] : access_point.provider_connection_id
+  ]  
+}
+
+#vipin - to accept the AWS Dx connection 
+resource "aws_dx_connection_confirmation" "ToAccepttheDxconnectioninAWSside" {
+  connection_id = local.provider_connection_ids[0]
+}
+
+#vipin - to fetch the VLAN ID of AWS Dx connection 
+locals {
+    vlan_tags = flatten([
+    for z in equinix_fabric_connection.L2_FCRSG_to_AWS.z_side : [
+      for ap in z.access_point : [
+        for lp in ap.link_protocol : lp.vlan_tag
+      ]
+    ]
+  ])
+
+    create_resource = length(local.vlan_tags) > 0
+}
+
+data "aws_dx_connection" "tofetchVLANID" {
+  depends_on = [ equinix_fabric_connection.L2_FCRSG_to_AWS ]
+  name = "L2_FCRSG_to_AWS"
+}
+
+output "vlan_id" {
+value = data.aws_dx_connection.tofetchVLANID.vlan_id
+}
+
+#vipin - to create Layer 3 on AWS VIF 
+
+resource "aws_dx_private_virtual_interface" "Create_AWS_SG_PrivateVIF" {
+  depends_on = [aws_dx_connection_confirmation.ToAccepttheDxconnectioninAWSside ]
+  connection_id    = local.provider_connection_ids[0] 
+  name             = "AWS_VIF_Creation_SG"
+  vlan             = data.aws_dx_connection.tofetchVLANID.vlan_id
+  address_family   = "ipv4"
+  bgp_asn          = 24116
+  bgp_auth_key     = "XXYYFFDDCC"
+  amazon_address   = "192.168.1.2/30"
+  customer_address = "192.168.1.1/30"
+  mtu              = 1500
+  vpn_gateway_id   = "vgw-09be1bd5f63e75f5c"
+} 
+
+#vipin - to create Layer 3 on BGP 
+resource "equinix_fabric_routing_protocol" "L3_FCRSG_to_AWS_Equinixside" {
+  connection_uuid = equinix_fabric_connection.L2_FCRSG_to_AWS.id
+  type            = "DIRECT"
+  name            = "L3_FCRSG_to_AWS_Equinixside"
+  direct_ipv4 {
+    equinix_iface_ip = "192.168.1.1/30"
+
+  }
+}
+
+resource "equinix_fabric_routing_protocol" "L3_FCRSG_to_AWS_AWSside" {
+  depends_on = [
+    equinix_fabric_routing_protocol.L3_FCRSG_to_AWS_Equinixside
+  ]
+  connection_uuid = equinix_fabric_connection.L2_FCRSG_to_AWS.id
+  type            = "BGP"
+  customer_asn    = 64520
+  name            = "L3_FCRSG_to_AWS_AWSside"
+  bgp_auth_key    = "XXYYFFDDCC"
+  bgp_ipv4 {
+    customer_peer_ip = "192.168.1.2"
+    enabled          = true
+  }
+
+}
+
+
+
 
 ##vipin - to create Azure Service Key 
+/*
 resource "azurerm_express_route_circuit" "ERSkey_Creation_process" {
   name                  = var.ERCircuitName
   resource_group_name   = var.Azureresourcegroupname
